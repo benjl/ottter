91384ae2d0b1c069c436b6d325860eb78036952a
Ben Cox <benjamin.jl.cox@gmail.com>
2013-01-26 11:21:27 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ottter::Application.config.secret_token = 'abdd7a0678d80758f8a427ae2753eae30f9947c00b9a8be23cdaeb2f64efd3c2ca8707920d0a71b9906f3d39015ef0bcc1952aa91c0e7294ab614631eb30d65d'
