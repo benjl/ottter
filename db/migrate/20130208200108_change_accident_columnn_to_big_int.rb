c891d3bef86590c523e6a9008bdea705b258edfd
Ben Cox <benjamin.jl.cox@gmail.com>
2013-02-08 15:02:53 -0500

    Changed Accident -> Tid from integer to bigint for deployment

---
+++ db/migrate/20130208200108_change_accident_columnn_to_big_int.rb
@@ -0,0 +1,9 @@
+class ChangeAccidentColumnnToBigInt < ActiveRecord::Migration
+  def up
+  	change_column :accidents, :tid, :bigint
+  end
+
+  def down
+  	change_column :accidents, :tid, :integer
+  end
+end
