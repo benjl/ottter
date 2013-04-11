16b5ebacb3ae4b5512d27a015479c3f4a420b222
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-11 13:26:59 -0400

    Improving UI and beginning validations and error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  respond_to :html,:xml,:json
 
   # GET /users
   # GET /users.json
@@ -43,25 +42,13 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-    if @user.save
-      respond_with do |format|
-        format.html do
-          if request.xhr?
-            render :partial => "users/confirm", :locals => { :user => @user }, :layout => false, :status => :created
-          else
-            redirect_to @user
-          end
-        end
-      end
-    else
-      respond_with do |format|
-        format.html do
-          if request.xhr?
-            render :json => @users.errors, :status => :unprocessable_entity
-          else
-            render :action => :new, :status => :unprocessable_entity
-          end
-        end
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to edit_user_path(@user), notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
