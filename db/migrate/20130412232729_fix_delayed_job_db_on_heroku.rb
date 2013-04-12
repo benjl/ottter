8ca2b448a0834ba24222641528c6d8ff6644fd58
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-12 19:28:22 -0400

    Fixing delayed jobs db on heroku

---
+++ db/migrate/20130412232729_fix_delayed_job_db_on_heroku.rb
@@ -0,0 +1,22 @@
+class FixDelayedJobDbOnHeroku < ActiveRecord::Migration
+  def up
+  	create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  end
+
+  def down
+  end
+end
