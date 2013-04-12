c434ccff08d32a5ddc039ef126bb20360655fd96
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-12 19:24:41 -0400

    Fixing path validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :phone, :street_orig, :street_dest, :path
 
-  validates :name,:name,:phone,:street_orig,:street_dest,:path, :presence => true
+  validates :name,:name,:phone,:street_orig,:street_dest, :presence => true
 
   validates :email,:phone, :uniqueness => true
-
 end
