f06f874fa8f163454717d95308d27c1317d84139
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-14 09:11:36 -0400

    Deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Ottter::Application.routes.draw do
 
   resources :users
 
-  root :to => 'welcomew#index'
+  root :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
