35a93496efde8e2827b4ef64411f0d36c4462ea8
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-21 21:43:17 -0400

    Yet another PG migration fix

--- db/migrate/20130420135156_add_password_to_users.rb
+++ db/migrate/20130420135156_add_password_to_users.rb
@@ -2,13 +2,13 @@ class AddPasswordToUsers < ActiveRecord::Migration
   def up
   	add_column :users, :encrypted_password, :string
   	add_column :users, :salt, :string
-  	remove_column :users, :name, :string
-  	remove_column :users, :email, :string
+  	remove_column :users, :name
+  	remove_column :users, :email
   end
 
   def down
-  	remove_column :users, :encrypted_password, :string
-  	remove_column :users, :salt, :string
+  	remove_column :users, :encrypted_password
+  	remove_column :users, :salt
   	add_column :users, :name, :string
   	add_column :users, :email, :string
   end
