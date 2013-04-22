21e72eacbaf3a886a2f04b5b36af74ce2cf643e9
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-21 21:40:13 -0400

    Another fix for PG migration

--- db/migrate/20130413190909_sched_mor_start_to_integer.rb
+++ db/migrate/20130413190909_sched_mor_start_to_integer.rb
@@ -1,9 +1,9 @@
 class SchedMorStartToInteger < ActiveRecord::Migration
   def up
-  	remove_column :users, :sched_mor_start, :datetime
-  	remove_column :users, :sched_mor_end, :datetime
-  	remove_column :users, :sched_eve_start, :datetime
-  	remove_column :users, :sched_eve_end, :datetime
+  	remove_column :users, :sched_mor_start
+  	remove_column :users, :sched_mor_end
+  	remove_column :users, :sched_eve_start
+  	remove_column :users, :sched_eve_end
     add_column :users, :sched_mor_start, :integer
     add_column :users, :sched_mor_end, :integer
     add_column :users, :sched_eve_start, :integer
@@ -15,9 +15,9 @@ class SchedMorStartToInteger < ActiveRecord::Migration
     add_column :users, :sched_mor_end, :integer
     add_column :users, :sched_eve_start, :integer
     add_column :users, :sched_eve_end, :integer
-    remove_column :users, :sched_mor_start, :datetime
-    remove_column :users, :sched_mor_end, :datetime
-    remove_column :users, :sched_eve_start, :datetime
-    remove_column :users, :sched_eve_end, :datetime
+    remove_column :users, :sched_mor_start
+    remove_column :users, :sched_mor_end
+    remove_column :users, :sched_eve_start
+    remove_column :users, :sched_eve_end
   end
 end
