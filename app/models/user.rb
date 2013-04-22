24b9f9dbf5843771c7fd1d502434b94834275126
Ben Cox <benjamin.jl.cox@gmail.com>
2013-04-21 21:37:57 -0400

    Fixing migration for PG

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
-  PHONE_REGEX = /^[1][0-9]{10}$/
-  attr_accessible :password, :password_confirmation, :phone, :street_orig, :street_dest, :path, :sched, :sched_mor_start, :sched_mor_end, :sched_eve_start, :sched_eve_end, :sens
+	attr_accessor :password
+	PHONE_REGEX = /^[1][0-9]{10}$/
+	attr_accessible :password, :password_confirmation, :phone, :street_orig, :street_dest, :path, :sched, :sched_mor_start, :sched_mor_end, :sched_eve_start, :sched_eve_end, :sens
 
-  validates :phone, :presence => true, :uniqueness => true, :format => PHONE_REGEX
+	validates :phone, :presence => true, :uniqueness => true, :format => PHONE_REGEX
 
-  validates :street_orig, :presence => true, :on => :edit
-  validates :street_dest, :presence => true, :on => :edit
+	validates :street_orig, :presence => true, :on => :edit
+	validates :street_dest, :presence => true, :on => :edit
 
-  validates :password, :confirmation => true #password_confirmation attr
-  validates_length_of :password, :in => 6..20, :on => :create
+	validates :password, :confirmation => true #password_confirmation attr
+	validates_length_of :password, :in => 6..20, :on => :create
 
 
-  before_save :encrypt_password
-  after_save :clear_password
+	before_save :encrypt_password
+	after_save :clear_password
 
 	def match_password(login_password="")
 		encrypted_password === BCrypt::Engine.hash_secret(login_password, salt)
--- db/migrate/20130413190909_sched_mor_start_to_integer.rb
+++ db/migrate/20130413190909_sched_mor_start_to_integer.rb
@@ -1,15 +1,23 @@
 class SchedMorStartToInteger < ActiveRecord::Migration
   def up
-  	change_column :users, :sched_mor_start, :integer
-  	change_column :users, :sched_mor_end, :integer
-  	change_column :users, :sched_eve_start, :integer
-  	change_column :users, :sched_eve_end, :integer
+  	remove_column :users, :sched_mor_start, :datetime
+  	remove_column :users, :sched_mor_end, :datetime
+  	remove_column :users, :sched_eve_start, :datetime
+  	remove_column :users, :sched_eve_end, :datetime
+    add_column :users, :sched_mor_start, :integer
+    add_column :users, :sched_mor_end, :integer
+    add_column :users, :sched_eve_start, :integer
+    add_column :users, :sched_eve_end, :integer
   end
 
   def down
-  	change_column :users, :sched_mor_start, :datetime
-  	change_column :users, :sched_mor_end, :datetime
-  	change_column :users, :sched_eve_start, :datetime
-  	change_column :users, :sched_eve_end, :datetime
+    add_column :users, :sched_mor_start, :integer
+    add_column :users, :sched_mor_end, :integer
+    add_column :users, :sched_eve_start, :integer
+    add_column :users, :sched_eve_end, :integer
+    remove_column :users, :sched_mor_start, :datetime
+    remove_column :users, :sched_mor_end, :datetime
+    remove_column :users, :sched_eve_start, :datetime
+    remove_column :users, :sched_eve_end, :datetime
   end
 end
