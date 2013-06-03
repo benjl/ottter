3e3e981ae5e3cdf4815a7f2fc6f92d20f91281f0
Ben <benjamin.jl.cox@gmail.com>
2013-06-03 14:09:49 -0300

    Adding some tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,30 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "user has a phone number" do
+    user = User.new
+    assert user.phone?
+  end
+  
+  test "user has a valid phone number" do
+    user = User.new
+    assert user.phone.length === "10"
+  end
+  
+  test "user has a street_orig" do
+    user = User.new
+   assert user.street_dest?
+  end
+  
+  test "user has a street_dest" do
+    user = User.new
+    assert_match user.street_orig?
+  end
+  
+  test "user password is between 6 and 20 characters" do
+    user = User.new
+    assert_match(/.{6,20}/, user.password)
+  end
 end
+
